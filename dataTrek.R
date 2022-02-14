d774a916bad3a3bc113d2b9fec01c7b5781f96d1
Mathieu Pierre <pierremathieu18@gmail.com>
2022-02-13 19:57:24 -0500

    script modified

--- dataTrek.R
+++ dataTrek.R
@@ -1,3 +1,9 @@
+#install.packages("here")
+#install.packages("sf")
+#install.packages("rnaturalearth")
+#install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
+library(sf)
+library(rnaturalearth)
 library(tidyverse)
 library(ggthemes)
 library(scales)
@@ -5,6 +11,11 @@ library(GGally)
 library(paletteer)
 library(rmarkdown)
 library(xaringan)
+library(here)
+library("ggpubr")
+here("data")
+getwd()
+setwd("C:/Users/PROF-ENS/Desktop/data_trek")
 
 euka_ppe <- read.csv("data/euk_prok_stacked.csv")
 euka_ppe
@@ -57,8 +68,9 @@ p <- ggplot(
   )
 ) +
   geom_col()
+p
 
-ggsave("C:\\Users\\PROF-ENS\\Desktop\\data_trek\\results\\box_plot.png", p, width = 5.97, height = 4.79, dpi = 300)
+ggsave("results\\box_plot.png", p, width = 5.97, height = 4.79, dpi = 300)
 
 #=============================================================================#   
 #transforming variables values in percentage to calculate relative abundance
@@ -100,22 +112,150 @@ fig1 <- ggplot(data_pivot, aes(fill = Pp_types, x = Samples_ID, y = abundance)) 
        fill = "Picoeukarytes\ntypes"
   )
 fig1
-ggsave("C:\\Users\\PROF-ENS\\Desktop\\data_trek\\results\\bar_plot.png", fig1, width = 5.97, height = 4.79, dpi = 300)
+ggsave("results/bar_plot.png", fig1, width = 5.97, height = 4.79, dpi = 300)
 
 
 
 #====================#
 # Save CSV file
 #===================#
-write.csv(ppe_stacked, "C:\\Users\\PROF-ENS\\Desktop\\data_trek\\data\\pico_stacked.csv", row.names = FALSE )
+write.csv(ppe_stacked, "data/pico_stacked.csv", row.names = FALSE )
 
 #===============================#
 # import pp_en_features file
 #===============================#
 pp_env <- read.csv("data/pp_env_features.csv")
+head(pp_env)
 ppe_env <- pp_env %>% 
   select(-Samples_ID, -Legend)
-ppe_env
+summary(ppe_env)
+
+#Plot1
+
+plot1 <- ggplot(
+  data = ppe_env,
+  aes(
+    y = PPE,
+    x = Depth,
+  )
+) +
+  stat_smooth(method = lm, se = FALSE, color = "black", formula = y ~ x) +
+  geom_point() +
+  labs(
+    x = "Depth (m)",
+    y = "PPE abundance (%)"
+  ) +
+  stat_cor(
+    aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+    label.x = 350
+  ) 
+  
+
+# Plot O2
+plot2 <- ggplot(
+  data = ppe_env,
+  aes(
+    y = PPE,
+    x = O2,
+  )
+) +
+  stat_smooth(method = lm, se = FALSE, color = "black", formula = y ~ x)+
+  geom_point() +
+  labs(
+    x = "O2 (umol/L)",
+    y = "PPE abundance (%)"
+  ) +
+  
+stat_cor(
+  aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+  label.x = 13
+) 
+
+#Plot salinity
+plot3 <- ggplot(
+  data = ppe_env,
+  aes(
+    y = PPE,
+    x = Salinity,
+  )
+) +
+  stat_smooth(method = lm, se = FALSE, color = "black", formula = y ~ x)+
+  geom_point() +
+  labs(
+    x = "Salinity (PSU)",
+    y = "PPE abundance (%)"
+  ) +
+stat_cor(
+  aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+  label.x = 37
+) 
+
+#Plot NO2
+plot4 <- ggplot(
+  data = ppe_env,
+  aes(
+    y = PPE,
+    x = NO2,
+  )
+) +
+  stat_smooth(method = lm, se = FALSE, color = "black", formula = y ~ x)+
+  geom_point() +
+  labs(
+    x = "NO2 (umol/L)",
+    y = "PPE abundance (%)"
+  ) +
+stat_cor(
+  aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+  label.x = .4
+) 
+
+#Plot NO2NO3
+plot5 <- ggplot(
+  data = ppe_env,
+  aes(
+    y = PPE,
+    x = NO2NO3,
+  )
+) +
+  stat_smooth(method = lm, se = FALSE, color = "black", formula = y ~ x)+
+  geom_point() +
+  labs(
+    x = "NO2NO3 (umol/L)",
+    y = "PPE abundance (%)"
+  ) +
+stat_cor(
+  aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+  label.x = 10
+) 
+
+#Plot NO2NO3
+plot6 <- ggplot(
+  data = ppe_env,
+  aes(
+    y = PPE,
+    x = PO4,
+  )
+) +
+  stat_smooth(method = lm, se = FALSE, color = "black", formula = y ~ x)+
+  geom_point() +
+  labs(
+    x = "PO4 (umol/L)",
+    y = "PPE abundance (%)"
+  ) +
+  
+stat_cor(
+  aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+  label.x = 1
+) 
+
+figure <- ggarrange(plot1, plot2, plot3, plot4, plot5, plot6,
+                    ncol = 2, nrow = 3)
+figure
+ggsave("results/ppe_features_plot.png", figure, width = 5.97, height = 4.79, dpi = 300)
+
+
+reglinaire <- lm(PPE ~ Depth, data = ppe_env)
+summary(reglinaire)
 
 #=====================================#
 #Transfom dataframe in a longer form
@@ -145,7 +285,7 @@ fig2 <- ggplot(
   facet_wrap(~param_var, scales = "free_x") +
   facet_wrap(~param_var, scales = "free_y")
 fig2
-ggsave("C:\\Users\\PROF-ENS\\Desktop\\data_trek\\results\\ppe_env_point.png", fig2, width = 5.97, height = 4.79, dpi = 300)
+ggsave("/results/ppe_env_feature_point.png", fig2, width = 5.97, height = 4.79, dpi = 300)
 
 #==================================#
 # Import geospatial data file
@@ -153,3 +293,6 @@ ggsave("C:\\Users\\PROF-ENS\\Desktop\\data_trek\\results\\ppe_env_point.png", fi
 pp_abundance <- read.csv("data/ppe_abundance.csv")
 head(pp_abundance)
 glimpse(pp_abundance)
+
+
+
